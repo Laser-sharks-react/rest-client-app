c6f61bf277b7c8d5be595e4121a17ae151461588
Анастасия <nastya.miss.7@gmail.com>
2025-09-03 01:16:07 +0400

    chore: change lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,8 @@ import { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import { FlatCompat } from '@eslint/eslintrc';
 import prettierPlugin from 'eslint-plugin-prettier';
+import tseslint from 'typescript-eslint';
+import unicorn from 'eslint-plugin-unicorn';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,6 +14,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+
   {
     ignores: [
       'node_modules/**',
@@ -22,17 +25,62 @@ const eslintConfig = [
       '.idea/**',
       '.vscode/**',
       'next-env.d.ts',
+      'postcss.config.mjs',
     ],
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    ...tseslint.configs.recommendedTypeChecked[0],
+    ...tseslint.configs.recommendedTypeChecked[1],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
   },
   {
     plugins: {
       prettier: prettierPlugin,
+      unicorn,
     },
     rules: {
       '@typescript-eslint/no-unused-vars': [
         'warn',
         { argsIgnorePattern: '^_' },
       ],
+      '@typescript-eslint/consistent-type-assertions': [
+        'error',
+        { assertionStyle: 'never' },
+      ],
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/explicit-member-accessibility': [
+        'error',
+        { accessibility: 'explicit', overrides: { constructors: 'off' } },
+      ],
+      '@typescript-eslint/member-ordering': 'error',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+
+      'class-methods-use-this': 'off',
+
+      'unicorn/max-func-body-length': 'off',
+      'unicorn/no-empty-file': 'off',
+      'unicorn/prefer-query-selector': 'off',
+      'unicorn/filename-case': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+      'unicorn/no-useless-undefined': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/no-array-for-each': 'off',
+      'unicorn/no-array-callback-reference': 'off',
+      'unicorn/no-negated-condition': 'off',
+      'unicorn/no-lonely-if': 'off',
+      'unicorn/no-array-reduce': 'off',
       'prettier/prettier': 'warn',
     },
   },
