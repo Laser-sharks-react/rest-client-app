783a4471d77de293379d99d8bca2499cda4a23ca
Ulistonee <ulistonee@gmail.com>
2025-09-04 22:39:07 +0500

    feat: add firebase integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { CssBaseline } from '@mui/material';
+// import { CssBaseline } from '@mui/material';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
