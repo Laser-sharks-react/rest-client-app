b53d3f858d36d38a080119333350d60c446e3b37
Anastasiia <starkoans@gmail.com>
2025-09-04 14:08:46 +0500

    fix: tests with next-intl

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,9 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import HomePage from '@/app/[locale]/page';
+import { NextIntlClientProvider } from 'next-intl';
+import messages from '../../messages/en.json';
 
 describe('HomePage', () => {
   it('renders intro text', () => {
-    render(<HomePage />);
-    expect(screen.getByText(/get started by editing/i)).toBeInTheDocument();
+    render(
+      <NextIntlClientProvider locale="en" messages={messages}>
+        <HomePage />
+      </NextIntlClientProvider>
+    );
+    expect(screen.getByText(messages.HomePage.title)).toBeInTheDocument();
   });
 });
