1403073f80734ec70f0f0429cd747726501647d6
Ulistonee <ulistonee@gmail.com>
2025-09-05 00:41:37 +0500

    feat: add test login

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,16 @@
 import '@testing-library/jest-dom';
+import fetch, { Headers, Request, Response } from 'node-fetch';
+
+declare global {
+  var fetch: typeof fetch;
+  var Headers: typeof Headers;
+  var Request: typeof Request;
+  var Response: typeof Response;
+}
+
+if (!global.fetch) {
+  global.fetch = fetch;
+  global.Headers = Headers;
+  global.Request = Request;
+  global.Response = Response;
+}
