35a887f3a7e4a1019c368ef2e61d094bad6655e0
Ulistonee <ulistonee@gmail.com>
2025-09-05 01:02:41 +0500

    feat: add test login

--- src/__tests__/home.test.tsx
+++ src/__tests__/home.test.tsx
@@ -1,6 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import HomePage from '@/app/page';
 
+jest.mock('@/firebase', () => ({
+  auth: {},
+  registerWithEmailAndPassword: jest.fn(),
+}));
+
+jest.mock('react-firebase-hooks/auth', () => ({
+  useAuthState: () => [null, false, null],
+}));
+
 describe('HomePage', () => {
   it('renders intro text', () => {
     render(<HomePage />);
