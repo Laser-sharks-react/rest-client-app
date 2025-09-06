f78af4d48e9eb3f077c73423920e9089c359baeb
Анастасия <nastya.miss.7@gmail.com>
2025-09-06 22:08:13 +0400

    feat: add stylization for signup page

--- src/app/[locale]/signup/page.tsx
+++ src/app/[locale]/signup/page.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { Button, Container, TextField, Typography } from '@mui/material';
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { useTranslations } from 'next-intl';
 import { Link as IntlLink } from '@/i18n/navigation';
 import Link from '@mui/material/Link';
@@ -30,39 +37,65 @@ export default function SignUp() {
   };
 
   return (
-    <Container className="h-screen">
-      <Typography variant="h4" component="h2" gutterBottom>
-        {t('title')}
-      </Typography>
-      <TextField
-        variant="outlined"
-        value={name}
-        onChange={e => setName(e.target.value)}
-        label="Full Name"
-      />
-      <TextField
-        type="email"
-        value={email}
-        onChange={e => setEmail(e.target.value)}
-        label="E-mail Address"
-      />
-      <TextField
-        type="password"
-        value={password}
-        onChange={e => setPassword(e.target.value)}
-        label="Password"
-      />
-      <Button
-        className="px-6 py-3 text-black rounded-lg shadow-md bg-grey"
-        onClick={handleSignUp}
-        disabled={loading}
-      >
-        {t('signup')}
-      </Button>
-      <Typography>{t('alreadyHaveAccount')}</Typography>
-      <Link component={IntlLink} href={ROUTES.login}>
-        {t('login')}
-      </Link>
+    <Container className="h-screen flex items-center justify-center p-4">
+      <div className="w-full max-w-md space-y-4">
+        <Typography
+          variant="h4"
+          component="h2"
+          gutterBottom
+          className="text-[22px] leading-snug text-black"
+        >
+          {t('title')}
+        </Typography>
+        <form className="rounded-xl border border-zinc-300 p-5 space-y-3">
+          <FormControl fullWidth>
+            <FormLabel>Full Name</FormLabel>
+            <TextField
+              variant="outlined"
+              value={name}
+              onChange={e => setName(e.target.value)}
+              size="small"
+            />
+          </FormControl>
+
+          <FormControl fullWidth>
+            <FormLabel>E-mail Address</FormLabel>
+            <TextField
+              type="email"
+              variant="outlined"
+              value={email}
+              onChange={e => setEmail(e.target.value)}
+              size="small"
+            />
+          </FormControl>
+
+          <FormControl fullWidth>
+            <FormLabel>Password</FormLabel>
+            <TextField
+              type="password"
+              variant="outlined"
+              value={password}
+              onChange={e => setPassword(e.target.value)}
+              size="small"
+            />
+          </FormControl>
+
+          <Button
+            className="px-6 py-3 text-black rounded-lg shadow-md bg-grey"
+            onClick={handleSignUp}
+            disabled={loading}
+            variant="contained"
+          >
+            {t('signup')}
+          </Button>
+          <Typography>
+            {t('alreadyHaveAccount')}
+            <Link component={IntlLink} href={ROUTES.login} className="ml-4">
+              {t('login')}
+            </Link>
+          </Typography>
+        </form>
+      </div>
     </Container>
   );
 }
