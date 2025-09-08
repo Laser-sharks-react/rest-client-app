ef7bb3f06dab12741a35b90f08bf605664c50b9d
Anastasiia <starkoans@gmail.com>
2025-09-08 19:55:24 +0500

    refactor: session token utils

--- src/utils/session-token.ts
+++ src/utils/session-token.ts
@@ -1,10 +1,25 @@
-import { COOKIE_TIME, COOKIES } from '@/sources/constants';
+import { ALG, COOKIE_TIME, COOKIES } from '@/sources/constants';
+import { type JWTPayload, SignJWT } from 'jose';
 import { type NextResponse, type NextRequest } from 'next/server';
 
-export const getSessionCookie = (req: NextRequest): string | null =>
+const secret = new TextEncoder().encode(process.env.SESSION_SECRET!);
+
+export async function createSessionToken(
+  payload: JWTPayload,
+  maxAgeSec = COOKIE_TIME
+) {
+  const now = Math.floor(Date.now() / 1000);
+  return await new SignJWT(payload)
+    .setProtectedHeader({ alg: ALG })
+    .setIssuedAt(now)
+    .setExpirationTime(now + maxAgeSec)
+    .sign(secret);
+}
+
+export const getSessionToken = (req: NextRequest): string | null =>
   req.cookies.get(COOKIES.session)?.value ?? null;
 
-export const setSessionCookie = (
+export const setSessionToken = (
   res: NextResponse,
   token: string,
   maxAge = COOKIE_TIME
