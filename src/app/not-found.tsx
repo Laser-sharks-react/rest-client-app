d53e5945b00cf3e5812087bd9816dc017e5cc4ea
Анастасия <nastya.miss.7@gmail.com>
2025-09-10 00:28:54 +0400

    fix: fix displaying custom 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,6 +14,7 @@ export default async function NotFound() {
       <NextLink href={ROUTES.home} className="text-blue-500 hover:underline">
         {t('goHome')}
       </NextLink>
+      </NextLink>
     </Container>
   );
 }
