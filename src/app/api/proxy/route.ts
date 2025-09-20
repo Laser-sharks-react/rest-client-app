b78bedb6925b5e79946adb310e9dbaf7ce66e8b1
Ulistonee <ulistonee@gmail.com>
2025-09-20 21:11:39 +0500

    fix: body in get requests

--- src/app/api/proxy/route.ts
+++ src/app/api/proxy/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: NextRequest) {
     const res = await fetch(url, {
       method,
       headers,
-      body: method !== DEFAULT_HTTP_METHOD ? body : '',
+      ...(method !== DEFAULT_HTTP_METHOD ? { body } : {}),
     });
 
     const arrayBuffer = await res.arrayBuffer();
